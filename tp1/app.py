5cdabecdede70b8d3a90bfeeff4a4c0adb5f0a4b
Esteban <estebanarivasv@gmail.com>
2020-10-03 16:30:32 -0300

    Initial files

--- tp1/app.py
+++ tp1/app.py
@@ -1,2 +1,3 @@
 if __name__ == '__main__':
     print("Welcome to a homemade lexical analyzer!")
+    
---
+++ tp1/matchers/emailMatcher.py
@@ -0,0 +1,4 @@
+""" Email (nombre@dominio)
+2. URL (http-https://www…..)
+3. Dirección IPv4 (000.000.000.000-255.255.255.255)
+4. Contraseña segura"""
