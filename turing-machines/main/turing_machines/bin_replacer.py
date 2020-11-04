76cc36f5bb02394fa1901c2b9107d863584c3881
Esteban Rivas <estebanarivasv@gmail.com>
2020-11-04 00:38:54 -0300

    Ex 1 finished

--- turing-machines/main/turing_machines/bin_replacer.py
+++ turing-machines/main/turing_machines/bin_replacer.py
@@ -1,121 +1,140 @@
-from termcolor import cprint
-from terminaltables import AsciiTable
 from colorclass import Color
+from termcolor import cprint
+from terminaltables import SingleTable
+
+"""
+    Syntax:     states = [Dict, Dict, ..., Dict]
+
+    Each state will have:
+    {"name": "qWrite0",
+     "inputs":  {
+                 "0": "qWrite0",
+                 "1": "qWrite1"
+                },
+     "write": "0",
+     "move": "R"},
+
+"""
 
 
-class BinaryReplacer:
-    # The turing machine must change the ones for zeroes or viceversa
-    """
-    q0 - Initial state
-    q1 - If zero recieved, turns it to one
-    q2 - If one recieved, turns it to zero
-    q3 - Accepting state
-    q4 - Rejecting state
-    """
-
-    input_list = None
+class TuringMachine:
+    name = "Binary replacer"
     states = [
-        {"name": "q0", "0": "q1", "1": "q2", "finish": "q3", "error": "q4"},
-        {"name": "q1", "0": "q1", "1": "q2", "finish": "q3", "error": "q4"},
-        {"name": "q2", "0": "q1", "1": "q2", "finish": "q3", "error": "q4"}
+        # qWrite0 - Writes a 0
+        {"name": "qWrite0",
+         "inputs": {
+             "0": "qWrite1",
+             "1": "qWrite0"
+         },
+         "write": "0",
+         "move": "R"},
+        # qWrite1 - Writes a 1
+        {"name": "qWrite1",
+         "inputs": {
+             "0": "qWrite1",
+             "1": "qWrite0"
+         },
+         "write": "1",
+         "move": "R"}
     ]
-    control_position = 3
-    tape = None
-    state_index = 0
-
-    def print_tape(self, position, situation):
-        printable_tape = self.tape
-        print(printable_tape)
-        if situation == "R":  # RIGHT
-
-            # Defines the tape with the current position of the control colored
-            printable_tape[position] = Color('{autogreen}' + self.tape[position] + '{/green}')
-
-            tape_table = AsciiTable([printable_tape])
-            cprint("Tape state: ")
-            print(tape_table.table)
-
-        elif situation == "B":  # BAD
-            # Defines the tape with the current position of the control colored
-            printable_tape[position] = Color('{autored}' + self.tape[position] + '{/red}')
-
-            tape_table = AsciiTable([printable_tape])
-            cprint("Tape state: ")
-            print(tape_table.table)
+    input = []  # Input list from given string
+    tape = []  # len(input) + 6
+    control_position = 1  # Position of the tm's control head
+    current = ""  # Describes the current state index
+    c_state = {}
+    reject = False
 
     def set_input(self, string):
 
-        entered_list = list(string)
+        self.input = list(string)
+        self.input.insert(0, "")
+        self.input.append("")
 
-        entered_list.insert(0, "")
-        entered_list.insert(0, "")
-        entered_list.insert(0, "")
-        entered_list.append("")
-        entered_list.append("")
-        entered_list.append("")
+        self.tape = list(string)
+        # Add this in order to simulate the infinite tape
+        self.tape.insert(0, "")
+        self.tape.append("")
 
-        self.input_list = list(string)
-        self.tape = self.input_list
+    def get_state(self) -> dict:
+        if self.current == "qWrite0":
+            cprint("\nCurrent state: qWrite0", "yellow")
+            return self.states[0]
+        if self.current == "qWrite1":
+            cprint("\nCurrent state: qWrite1", "yellow")
+            return self.states[1]
 
-    @staticmethod
-    def index_setter(next_state):
-        if next_state == "q1":
-            return 1
-        elif next_state == "q2":
-            return 2
+    def print_tape(self):
+        printable_tape = self.tape
+        printable_tape[self.control_position] = Color('{green}' + self.tape[self.control_position] + '{/green}')
+        tape_table = SingleTable([printable_tape])
+        print("\n\n---------------------------------------------------------------\n\n")
+        print("TURING MACHINE STATE: \n")
+        print(tape_table.table)
 
-    def write_tape(self, state, position):
-        if state == "q1":
-            # Defines the tape with the modified value colored
-            self.tape[position] = Color('{autoyellow}' + "1" + '{/yellow}')
-            tape_table = AsciiTable([self.tape])
-            cprint("Tape state with the table modified: ")
-            print(tape_table.table)
+    """
+    This function recieves the char and changes the state
 
-        elif state == "q2":
-            # Defines the tape with the modified value colored
-            self.tape[position] = Color('{autoyellow}' + "0" + '{/yellow}')
-            tape_table = AsciiTable([self.tape])
-            cprint("Tape state with the table modified: ")
-            print(tape_table.table)
+    """
+
+    def read_input(self, char):
+        self.print_tape()
+        print(f"\nReading from control head: {char}")
+        # Print tape
+
+        self.c_state = self.get_state()  # Obtains dict form self.current
+        if char == "0":
+            cprint(f"\n'0' -> {self.c_state['inputs']['0']}", 'cyan')
+            self.current = self.c_state['inputs']['0']
+        elif char == "1":
+            cprint(f"\n'1' -> {self.c_state['inputs']['1']}", 'cyan')
+            self.current = self.c_state['inputs']['1']
+        else:
+            cprint("\nWrong input, moving to qReject", "red")
+            cprint("\nqReject reached", "yellow")
+            print("\n\n---------------------------------------------------------------\n\n")
+            self.reject = True
+
+    def write_tape(self):
+
+        self.c_state = self.get_state()
+        print(f"\nWriting {self.c_state['write']} instead of {self.tape[self.control_position]}")
+
+        self.tape[self.control_position] = Color('{yellow}' + self.c_state['write'] + '{/yellow}')
+
+    def move_tape(self):
+        if self.c_state['move'] == "R":  # RIGHT
+            self.control_position += 1
+            cprint("\nMoving right...", "cyan")
+        elif self.c_state['move'] == "L":  # LEFT
+            self.control_position -= 1
+        elif self.c_state['move'] == "H":  # HALT
+            pass
 
     def run_tm(self):
+        """
+        1 - Estado actual recibe entrada y cambia de estado
+        2 - Estado cambiado escribe en cinta
+        """
 
-        while self.control_position <= (len(self.tape) - 3):
-
-            # Save the binary value that is being focused by the tape control
+        while not self.tape[self.control_position] == "" and self.reject is False:
             char = self.tape[self.control_position]
-
-            # Print of the actual state of the tape
-            self.print_tape(self.control_position, "R")
-
-            # Set the current state by index
-            current_state = self.states[self.state_index]
-            cprint(f"Actual state: {current_state['name']}")
-
-            if char == "0":
-                self.state_index = self.index_setter(current_state['0'])
-                print(f"Move from {current_state['name']} to {current_state['0']}")
-                self.write_tape(current_state['0'], self.control_position)
-
-            elif char == "1":
-                self.state_index = self.index_setter(current_state['1'])
-
-            else:
-                self.print_tape(self.control_position, "B")
-                print(f"Move from {current_state['name']} to final rejecting state {current_state['error']}.")
+            self.read_input(char)
+            if self.reject is True:
                 break
+            else:
+                self.write_tape()
+                self.move_tape()
 
-        cprint("Accepting state q4 reached. The string is valid and correctly swapped", "green")
+        if self.reject is False:
+            cprint(f"\nThis is the end of string, moving to qAccept", "green")
+            cprint(f"\nqAccept reached", "yellow")
+            self.print_tape()
+            print("\n\n---------------------------------------------------------------\n\n")
 
-        input_table = AsciiTable([self.input_list])
-        print("Input: \n", input_table.table)
-
-        output_table = AsciiTable([self.tape])
-        print("Output: \n", output_table.table)
 
 if __name__ == '__main__':
-
-    tm = BinaryReplacer()
-    tm.set_input("00110100101")
+    # The turing machine must change the ones for zeroes or viceversa
+    tm = TuringMachine()
+    tm.set_input("00110000101")
+    tm.current = "qWrite0"  # Set as initial state
     tm.run_tm()
