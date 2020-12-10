4e455149eb0e276b07efd5ac99c44659d5c1b6de
Esteban Rivas <estebanarivasv@gmail.com>
2020-12-10 19:41:00 -0300

    Data insertion finished

--- wifi-users/main/__init__.py
+++ wifi-users/main/__init__.py
@@ -2,11 +2,23 @@ import os
 from dotenv import load_dotenv
 from flask import Flask
 
+from main.extensions import db
+
 # Loading environment variables
 load_dotenv()
+
+DB_PATH = os.getenv('DB_PATH')
+DB_NAME = os.getenv('DB_NAME')
+DB_URI = "sqlite:////" + DB_PATH + DB_NAME
+
 
 def create_app():
     # Flask application instance
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+    db.init_app(app)
 
     return app
--- wifi-users/main/services/data_insertion.py
+++ wifi-users/main/services/data_insertion.py
@@ -7,7 +7,9 @@ load_dotenv()
 DATA_PATH = os.getenv('DATA_PATH')
 
 
-def insert_data_to_db():
-
+def insert_data_to_db(db):
     dataframe = pd.read_csv(DATA_PATH, sep=';')
-    print(dataframe)
+    try:
+        dataframe.to_sql(name='Connection', con=db.engine, index=False)
+    except ValueError:
+        pass
