ef9b899bd371904021a3da235d2dc24cc860513b
Esteban Rivas <estebanarivasv@gmail.com>
2020-12-11 02:25:40 -0300

    Data queries working. Filters left

--- wifi-users/main/forms/filters.py
+++ wifi-users/main/forms/filters.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
+from wtforms.validators import Regexp
 
 
 class FilterForm(FlaskForm):
@@ -9,11 +10,10 @@ class FilterForm(FlaskForm):
         Regexp('\d\d/\d\d/\d\d\d\d \d\d:\d\d', message="Date must be in this format DD/MM/YYYY HH:MM")
     ])
     ap_mac = StringField('AP MAC address', validators=[
-        MacAddress('(?:[0-9a-fA-F]{2}\-){5}[0-9a-fA-F]{2}(:UM)?', message="AP MAC address must be in this format 04-18-D6-22-94-E7:UM")
+        Regexp('(?:[0-9a-fA-F]{2}\-){5}[0-9a-fA-F]{2}(:UM)?', message="AP MAC address must be in this format 04-18-D6-22-94-E7:UM")
     ])
     client_mac = StringField('Client MAC address', validators=[
-        RegMacAddressexp('(?:[0-9a-fA-F]{2}\-){5}[0-9a-fA-F]{2}(:UM)?', message="Client MAC address must be in this format 04-18-D6-22-94-E7")
+        Regexp('(?:[0-9a-fA-F]{2}\-){5}[0-9a-fA-F]{2}(:UM)?', message="Client MAC address must be in this format 04-18-D6-22-94-E7")
     ])
     submit = SubmitField('Filter')
-
 
